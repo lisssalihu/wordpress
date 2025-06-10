3e30b2780ea683e31871216e8fa0def4c400b38c
lisssalihu <167891216+lisssalihu@users.noreply.github.com>
2025-06-10 07:03:26 -0700

    lesson2

--- functions.php
+++ functions.php
@@ -7,7 +7,37 @@ function load_scripts(){
         array(),
         filemtime(get_template_directory() .'/style.css'),'all'
     );
+    wp_enqueue_script('dropdown',get_template_directory().'/js/dropdown.js', array(),'1.0',false);
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
+
+function config (){
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu'=>'Main Menu',
+            'wp_devs_footer_menu'=>'Footer Menu',
+     )
+    );
+
+    $args=array(
+        'height'=> 225,
+        'width'=> 1920
+    );
+
+    add_theme_support('custom-header', $args);
+    add_theme_support('post-thumbnails'); 
+    add_theme_support('custom-logo', array(
+        'height' => 110,
+        'width' => 200,
+        'flex-height' => true,
+        'flex-width' => true,
+    ));
+    add_theme_support('automatic-feed-links');
+    add_theme_support('html5', array('comment-list','comment-form','search-form','gallery','caption','style','script'));
+    add_theme_support('title-tag');
+
+}
+add_action('after_setup_theme', 'config');
+
 
 ?>
