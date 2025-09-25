bc5021cd9d88d027b8c80ce1874d92017797a29b
Lis Salihu <lissalihu2@gmail.com>
2025-09-25 07:04:35 -0700

    lisii

---
+++ page-about.php
@@ -0,0 +1,52 @@
+<?php get_header();?>
+
+
+<div id="content" class="site-content">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="container">
+                <h1>About us</h1>
+                <p>Welcome to out tech corner- where innovation meets explanation</p>
+            </div>
+        </section>
+
+        <section class="about-intro">
+            <div class="container">
+                <h2>Who We Are</h2>
+                <p>We are a team of developers, engineers and tech enthusiast who love to whrite about th fast-moving world technology</p>
+            </div>
+        </section>
+
+            <section class="about-mission">
+            <div class="container">
+                <h2>Our mission</h2>
+                <p>Our goal is simple:to empowe curios minds and passionate thechies by delivering accurate.usful and accsesible content</p>
+            </div>
+        </section>
+
+        <section class="about-what-we-cover">
+            <div class="container">
+                <h2>what we cover</h2>
+                <ul>
+                    <li>Programmig tutorials and tips</li>
+                    <li>Software and app review</li>
+                    <li>AI/ML and data science insights</li>
+                    <li>Web Development, DevOps and cloud computinf</li>
+                    <li>Trends in tech adn indrusty news</li>
+                </ul>
+
+            </div>
+
+        </section>
+
+           <section class="about-contact">
+            <div class="container">
+                <h2>Want to Connect?</h2>
+                <p>We love hearing from our readers! Reach out via our Page.Follow us on social media to stay updated</p>
+            </div>
+        </section>
+
+</main>
+
+</div>
+<?php get_footer();?>
